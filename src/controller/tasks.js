b08aeec246a7c3216641cc89d0dd100f6d3afa49
Johnny <espgoodlife22@gmail.com>
2026-06-15 13:02:55 +0300

    done

--- src/controller/tasks.js
+++ src/controller/tasks.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { Task } from '../db/models/Tasks.js';
 
 export const getTasks = async (req, res) => {
@@ -15,4 +16,34 @@ export const addTask = async (req, res) => {
   const body = req.body;
   const newTask = await Task.create(body);
   res.status(201).json(newTask);
+};
+
+export const removeTask = async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findByIdAndDelete(id);
+  res.json(task);
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const result = await Task.findByIdAndUpdate(id, body, {
+    returnDocument: 'after',
+  });
+  if (!result) {
+    throw createHttpError(404, "task isn't found");
+  }
+  res.json(result);
+};
+
+export const updateOrCreateTask = async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const result = await Task.findByIdAndUpdate(id, body, {
+    returnDocument: 'after',
+    upsert: true,
+    includeResultMetadata: true,
+  });
+  const isUpdated = result.lastErrorObject.updatedExisting;
+  res.status(isUpdated ? 200 : 201).json(result.value);
 };
--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 import 'dotenv/config';
-import { addTask, getTaskById, getTasks } from '../controller/tasks.js';
+import {
+  addTask,
+  getTaskById,
+  getTasks,
+  removeTask,
+  updateOrCreateTask,
+  updateTask,
+} from '../controller/tasks.js';
 
 const tasksRouter = Router();
 
@@ -9,5 +16,11 @@ tasksRouter.get('/tasks', getTasks);
 tasksRouter.get('/tasks/:id', getTaskById);
 
 tasksRouter.post('/tasks', addTask);
+
+tasksRouter.delete('/tasks/:id', removeTask);
+
+tasksRouter.patch('/task/:id', updateTask);
+
+tasksRouter.put('/tasks/:id', updateOrCreateTask);
 
 export default tasksRouter;
